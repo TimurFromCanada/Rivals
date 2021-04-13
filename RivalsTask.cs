7b515f52513c71cd8be4a045c788a401228bb7d9
timurfromcanada <timurfromcanada@gmail.com>
2021-04-13 10:23:53 +0500

    Implemented RivalsTask.cs

--- RivalsTask.cs
+++ RivalsTask.cs
@@ -12,7 +12,46 @@ namespace Rivals
 	{
 		public static IEnumerable<OwnedLocation> AssignOwners(Map map)
 		{
-			yield break;
+			var visited = new HashSet<Point>();
+			var queue = new Queue<Tuple<int, Point, int>>();
+
+			for (var i = 0; i < map.Players.Length; i++)
+			{
+				queue.Enqueue(Tuple.Create(i, new Point(map.Players[i].X, map.Players[i].Y), 0));
+			}
+
+			while (queue.Count != 0)
+			{
+				var previous = queue.Dequeue();
+				var point = previous.Item2;
+
+				if (point.X < 0
+					|| point.X >= map.Maze.GetLength(0)
+					|| point.Y < 0 || point.Y >= map.Maze.GetLength(1)
+					|| map.Maze[point.X, point.Y] == MapCell.Wall)
+				{
+					continue;
+				}
+
+				if (visited.Contains(point))
+					continue;
+
+				visited.Add(point);
+				yield return new OwnedLocation(previous.Item1, new Point(point.X, point.Y), previous.Item3);
+
+				for (var dy = -1; dy <= 1; dy++)
+					for (var dx = -1; dx <= 1; dx++)
+					{
+						if (dx != 0 && dy != 0)
+							continue;
+						else
+						{
+							queue.Enqueue(Tuple.Create(previous.Item1, new Point { X = point.X + dx, Y = point.Y + dy }, previous.Item3 + 1));
+						}
+					}
+			}
 		}
 	}
 }
+
+
